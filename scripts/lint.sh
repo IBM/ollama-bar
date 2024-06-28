fb9f58316eb8a41d0feeae13dc87a5e616b7fe6b
Gabe Goodhart <ghart@us.ibm.com>
2024-06-28 16:41:54 -0600

    main: Add project boilerplate for formatting and tests
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ scripts/lint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+fnames=""
+for fname in $@
+do
+    if [[ "$fname" == *".py" ]] || [ -d $fname ] && [[ "$fname" == "ollama_bar"* ]]
+    then
+        fnames="$fnames $fname"
+    else
+        echo "Ignoring non-library file: $fname"
+    fi
+done
+if [ "$fnames" == "" ]
+then
+    fnames="ollama_bar"
+fi
+
+ruff check $arg $fnames
