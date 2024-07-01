c4f2f080e9e34d4751d398caf8044dcc89cb4eb7
Gabe Goodhart <ghart@us.ibm.com>
2024-07-01 09:32:55 -0600

    main: Add support for py2app build
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ scripts/build_app.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+################################################################################
+# This script is used to create an executable version of Ollama Bar app for
+# Mac OS. It uses the "py2app" module to bundle all dependencies into a single
+# executable app.
+#
+# https://py2app.readthedocs.io/en/latest/tutorial.html#create-a-setup-py-file
+################################################################################
+
+# Clean existing build artifacts
+rm -rf build/ dist/ ollama_bar.egg-info/
+
+# Perform a clean build of the application
+python setup.py py2app -A
