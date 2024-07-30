f9e142dd698fc4fbf2d7767ee0f9161816fa4c7b
Gabe Goodhart <ghart@us.ibm.com>
2024-07-30 17:16:17 -0600

    OpenWebUI: Run Open WebUI in a thread on the same Start/Stop toggle as ollama
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- ollama_bar/ollama_bar_app.py
+++ ollama_bar/ollama_bar_app.py
@@ -1,7 +1,5 @@
 # Standard
 import os
-import pathlib
-import sys
 
 # Third Party
 import rumps
@@ -9,6 +7,7 @@ import rumps
 # Local
 from ollama_bar.command_display_window import CommandDisplayWindow
 from ollama_bar.process_monitor import ProcessMonitor
+from ollama_bar.webui import OpenWebUIWrapper
 
 
 class OllamaBarApp(rumps.App):
@@ -20,13 +19,13 @@ class OllamaBarApp(rumps.App):
             title="",
             icon=self._resource("ollama.png"),
         )
+        self._run_webui = False
+
         # Add start/stop menu
         self._on_icon = self._resource("on.svg")
         self._off_icon = self._resource("off.svg")
         self._menu.add(
-            rumps.MenuItem(
-                "Start", icon=self._off_icon, callback=self._start_stop_ollama
-            )
+            rumps.MenuItem("Start", icon=self._off_icon, callback=self._start_stop)
         )
         self._menu.add(None)
 
@@ -35,25 +34,16 @@ class OllamaBarApp(rumps.App):
         self._setup_process_menu_items(self._menu)
         self._menu.add(None)
 
-        # Add the options for Open WebUI
-        self._open_webui_cmd = (
-            "{} -c 'import open_webui; open_webui.serve(host=\"localhost\")'".format(
-                sys.executable
-            )
-        )
-        self._open_webui_menu = rumps.MenuItem("Open WebUI")
-        self._open_webui_menu.add(
+        # Add a toggle to enable/disable Open WebUI
+        self._menu.add(
             rumps.MenuItem(
-                "Start", icon=self._off_icon, callback=self._start_stop_webui
+                "Open WebUI", icon=self._on_icon, callback=self._toggle_webui
             )
         )
-        self._setup_process_menu_items(self._open_webui_menu, "open_webui")
-        self._menu.add(self._open_webui_menu)
-        self._menu.add(None)
 
-        # Placeholder for the running processes
+        # Placeholder for the running processes/threads
         self._ollama_server_proc = None
-        self._open_webui_proc = None
+        self._open_webui_wrapper = None
 
     def __del__(self):
         if self.running:
@@ -69,56 +59,40 @@ class OllamaBarApp(rumps.App):
 
     _RESOURCE_ROOT = os.path.join(os.path.dirname(__file__), "resources")
 
-    def _start_stop_ollama(self, sender: rumps.MenuItem | None):
-        self._ollama_server_proc = self._start_stop(
-            self._ollama_cmd,
-            self._ollama_server_proc,
-            self._stdout_window,
-            self._stderr_window,
-            sender,
-        )
-
-    def _start_stop_webui(self, sender: rumps.MenuItem | None):
-        data_dir = pathlib.Path.home() / ".open_webui"
-        data_dir.mkdir(parents=True, exist_ok=True)
-        env = os.environ.copy()
-        env.update({"WEBUI_AUTH": "false", "DATA_DIR": data_dir})
-        self._open_webui_proc = self._start_stop(
-            self._open_webui_cmd,
-            self._open_webui_proc,
-            self._open_webui_stdout_window,
-            self._open_webui_stderr_window,
-            sender,
-            env=env,
-        )
-
-    def _start_stop(
-        self,
-        command: str,
-        proc: ProcessMonitor | None,
-        stdout_window: CommandDisplayWindow,
-        stderr_window: CommandDisplayWindow,
-        sender: rumps.MenuItem | None,
-        **kwargs,
-    ) -> ProcessMonitor | None:
-        if proc is None:
+    def _start_stop(self, sender: rumps.MenuItem | None, **kwargs):
+        if self._ollama_server_proc is None:
             if sender is not None:
                 sender.title = "Stop"
                 sender.icon = self._on_icon
-            proc = ProcessMonitor(command, **kwargs)
-            stdout_window.set_process(proc)
-            stderr_window.set_process(proc)
-            proc.start()
+            self._ollama_server_proc = ProcessMonitor(self._ollama_cmd, **kwargs)
+            self._stdout_window.set_process(self._ollama_server_proc)
+            self._stderr_window.set_process(self._ollama_server_proc)
+            self._ollama_server_proc.start()
+            if self._run_webui:
+                self._open_webui_wrapper = OpenWebUIWrapper()
+                self._open_webui_wrapper.start()
         else:
             if sender is not None:
                 sender.title = "Start"
                 sender.icon = self._off_icon
-            proc.stop()
-            proc = None
-        return proc
+            self._ollama_server_proc.stop()
+            self._ollama_server_proc = None
+            if self._open_webui_wrapper is not None:
+                self._open_webui_wrapper.stop()
 
-    def _enable_disable_webui(self, sender: rumps.MenuItem | None) -> None:
-        """Enable/disable the open WebUI based on the button click"""
+    def _toggle_webui(self, sender: rumps.MenuItem):
+        if self._run_webui:
+            self._run_webui = False
+            sender.icon = self._off_icon
+            if self._open_webui_wrapper is not None:
+                self._open_webui_wrapper.stop()
+                self._open_webui_wrapper = None
+        else:
+            self._run_webui = True
+            sender.icon = self._on_icon
+            if self._ollama_server_proc is not None:
+                self._open_webui_wrapper = OpenWebUIWrapper()
+                self._open_webui_wrapper.start()
 
     def _setup_process_menu_items(self, parent: rumps.MenuItem, label: str = ""):
         """Helper to encapsulate adding menu items to manage stdout/stderr for
