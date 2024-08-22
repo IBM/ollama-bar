e34c3dfef94c8d55ab8a8d46a50586d462670f96
Gabe Goodhart <ghart@us.ibm.com>
2024-08-22 13:24:25 -0600

    test: Test cov for toggle logic for webui
    Branch: OpenWebUI
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- tests/test_ollama_bar_app.py
+++ tests/test_ollama_bar_app.py
@@ -47,3 +47,32 @@ def test_ollama_bar_stop_on_delete(subprocess_popen_mock, do_start):
     with mock.patch.object(app, "_start_stop") as start_stop:
         app.__del__()
         start_stop.called == do_start
+
+
+def test_ollama_bar_toggle_webui(subprocess_popen_mock):
+    """Test that toggling open webui on/off correctly recreates the wrapper and
+    toggles the icons
+    """
+    app = OllamaBarApp()
+    with (
+        mock.patch("ollama_bar.webui.OpenWebUIWrapper.start") as webui_start_mock,
+        mock.patch("ollama_bar.webui.OpenWebUIWrapper.stop") as webui_stop_mock,
+    ):
+        app._start_stop(None)
+        webui_start_mock.assert_called_once()
+        webui_stop_mock.assert_not_called()
+
+        # Toggle the webui while the app is running. Make sure it gets stopped
+        menu_mock = mock.MagicMock()
+        app._toggle_webui(menu_mock)
+        webui_stop_mock.assert_called_once()
+        assert menu_mock.icon is app._off_icon
+
+        # Toggle it back on and make sure start was re-called
+        app._toggle_webui(menu_mock)
+        assert len(webui_start_mock.mock_calls) == 2
+        assert menu_mock.icon is app._on_icon
+
+        # Turn off the main toggle and make sure the webui gets stopped
+        app._start_stop(None)
+        assert len(webui_stop_mock.mock_calls) == 2
